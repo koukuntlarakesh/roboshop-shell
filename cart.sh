da3aa0b4957837da02d7f821c262433dd79f04df
koukuntlarakesh <koukuntlarakesh@gmail.com>
2023-12-26 15:00:38 -0600

    roboshop-shell

--- cart.sh
+++ cart.sh
@@ -55,11 +55,11 @@ else
 }
 fi
 
-mkdir app &>>LOGFILE
+mkdir /app &>>LOGFILE
 VALIDATE $? "created directory"
 curl -L -o /tmp/cart.zip https://roboshop-builds.s3.amazonaws.com/cart.zip &>>LOGFILE
 VALIDATE $? "Downloading application code"
-cd app  &>>LOGFILE
+cd /app  &>>LOGFILE
 VALIDATE $? "Moving to app directory"
 unzip /tmp/cart.zip &>>LOGFILE
 VALIDATE $? "Extracting zip file"
