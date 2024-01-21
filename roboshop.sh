aedd232a1d29be37528840515d9c159f4e9b7bb4
koukuntlarakesh <koukuntlarakesh@gmail.com>
2024-01-21 13:25:18 -0600

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,15 +5,18 @@ SECURITY=sg-07e9d7ca57c23e404
 
 INSTANCES=("web" "cart" "user" "catalogue" "payment" "dispatch" "rabitmq" "shipping" "mongod" "mysql" "redis")
 
+
+
 for i in "${INSTANCES[@]}"
 do
-    echo "Instance $i is creating"
+ 
    if [ $i == "mongod" ] || [ $i == "mysql" ]||[ $i == "shipping" ]
    then
       INSTANCE_TYPE=t3.small
    else
       INSTANCE_TYPE=t2.micro
    fi
-   aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
-   echo " Instance $i is created done......"
+     PRIVATE_IP_ADD=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SECURITY --tag-specifications "ResourceType=instance,Tags=[{Key=webserver,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text) 
+        
+   echo " Instance $i:$PRIVATE_IP_ADD"
 done
